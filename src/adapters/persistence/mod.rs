2ccb6985c7b21c1c682372a606fc79f5c1778acc
Max Meindl <max_meindl@gmx.de>
2025-09-18 11:15:40 +0200

    Initial commit

---
+++ src/adapters/persistence/mod.rs
@@ -0,0 +1,14 @@
+use sqlx::PgPool;
+
+pub mod user;
+
+#[derive(Clone)]
+pub struct PostgresPersistence {
+    pool: PgPool,
+}
+
+impl PostgresPersistence {
+    pub fn new(pool: PgPool) -> Self {
+        PostgresPersistence { pool }
+    }
+}
